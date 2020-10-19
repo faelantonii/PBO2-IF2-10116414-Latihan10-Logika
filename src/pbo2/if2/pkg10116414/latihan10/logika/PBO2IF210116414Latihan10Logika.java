bc0b24c8340db2b97bf198ccd93949ca3bdab3e0
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:09:58 +0700

    Latihan 10 Program Program Logika

---
+++ src/pbo2/if2/pkg10116414/latihan10/logika/PBO2IF210116414Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.if2.pkg10116414.latihan10.logika;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Logika
+ */
+public class PBO2IF210116414Latihan10Logika {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 20;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
